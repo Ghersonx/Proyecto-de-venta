5f3c73c67886c34c6ba67d16d720f0ade265d4ac
Ghrsonx <ghersonacosta@unimayor.edu.co>
2023-12-24 23:19:29 +0100

    primer commit

---
+++ src/Modelo/Empleados.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Empleados {
+    private int id;
+    private String dni;
+    private String nombre;
+    private String telefono;
+    private String ocupacion;
+
+    public Empleados() {
+    }
+
+    public Empleados(int id, String dni, String nombre, String telefono, String ocupacion) {
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.ocupacion = ocupacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+   
+    
+}
